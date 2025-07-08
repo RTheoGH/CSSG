cf42a0fbb17cf8c3433ddcaa10aa52c2fbeaa8c2
TheoR <reynier.theo@gmail.com>
2025-07-08 22:05:55 +0200

    de la physique et des lettres (et la cam aussi)

---
+++ scripts/main.gd
@@ -0,0 +1,52 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	new_game()
+	#get_node("job").connect("game_over", Callable(self, "_on_game_over"))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var cam = get_node("Camera2D")
+	var player_pos = get_node("chomeur").global_position
+	var offset = Vector2.ZERO
+	
+	if Input.is_action_pressed("camera_right"):
+		offset.x += 50
+	if Input.is_action_pressed("camera_left"):
+		offset.x -= 50
+	if Input.is_action_pressed("camera_up"):
+		offset.y -= 50
+	if Input.is_action_pressed("camera_down"):
+		offset.y += 50
+		
+	var zoom = cam.zoom
+	if Input.is_action_just_released("camera_dezoom") and cam.zoom.x > 2.5 and cam.zoom.y > 2.5:
+		cam.zoom.x -= 0.1
+		cam.zoom.y -= 0.1
+		print(cam.zoom.x,",",cam.zoom.y)
+	if Input.is_action_just_released("camera_zoom") and cam.zoom.x < 3 and cam.zoom.y < 3:
+		cam.zoom.x += 0.1
+		cam.zoom.y += 0.1
+		print(cam.zoom.x,",",cam.zoom.y)
+	
+	var target_pos = player_pos + offset
+	var speed = 275.0
+	cam.global_position = cam.global_position.move_toward(target_pos,speed*delta)
+
+func new_game():
+	get_node("text").hide()
+	get_node("Button").hide()
+	get_node("chomeur").set_physics_process(true)
+	get_node("chomeur").global_position = Vector2(0,0)
+	#get_node("job").set_physics_process(true)
+	#get_node("job").global_position = Vector2(150,100)
+	
+func _on_game_over():
+	get_node("text").show()
+	get_node("Button").show()
+	get_node("chomeur").set_physics_process(false)
+	#get_node("job").set_physics_process(false)
+
+func _on_button_pressed() -> void:
+	new_game()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,45 @@
 extends CharacterBody2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var prev_position = Vector2()
 
 func get_input():
-	var direction := Input.get_vector("left", "right", "up", "down")
-	velocity = direction * SPEED
+	var direction := Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 func _physics_process(delta: float) -> void:
+	#_animated_sprite.play("default")q
+	if(prev_position.x>position.x):
+		_animated_sprite.scale.x = 1
+	if(prev_position.x<position.x):
+		_animated_sprite.scale.x = -1
+	prev_position = position
+	
+	if is_on_floor():
+		velocity.y = 0
+	else:
+		if Input.is_action_pressed("down"):
+			velocity += get_gravity() * delta * 2
+		else:
+			velocity += get_gravity() * delta
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
 	get_input()
 	move_and_slide()
+
+	if not is_on_floor():
+		if velocity.y < 0:
+			_animated_sprite.play("jump")
+		else:
+			_animated_sprite.play("fall")
+	elif abs(velocity.x) > 10:
+		_animated_sprite.play("walk")
+	else:
+		_animated_sprite.play("default")
