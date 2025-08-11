bb3af8ee0207f1c541e6c3f261e2cc44f6c00af1
VigKillian <viguier.killian05@gmail.com>
2025-08-11 17:30:35 +0200

    Suppression des cadres blancs sur les boutons

--- scripts/skills/pm.gd
+++ scripts/skills/pm.gd
@@ -4,6 +4,7 @@ extends Skill
 func _init() -> void:
 	target_type= Global.target.ALL_ENEMIES
 	skill_name = "Pavage et mélange"
+	description = "Attaque tous les ennemis"
 	MP = 10
 	accuracy = 1
 	
